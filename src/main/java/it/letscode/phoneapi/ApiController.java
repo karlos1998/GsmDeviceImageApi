0dee9323c4e2c404162515f998ea75b5b9f0f985
karlos3098 <fadeusz98@gmail.com>
2024-01-29 23:30:23 +0100

    find image by model

--- src/main/java/it/letscode/phoneapi/ApiController.java
+++ src/main/java/it/letscode/phoneapi/ApiController.java
@@ -1,14 +1,12 @@
 package it.letscode.phoneapi;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +18,15 @@ public class ApiController {
     public ResponseEntity<GsmArenaItem> getItemByModel(@RequestParam String model) {
         return repository.findByModel(model)
                 .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/find-image-by-model")
+    public ResponseEntity<byte[]> getImageByModel(@RequestParam String model) {
+        return repository.findByModel(model)
+                .map(item -> ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(item.getImageData()))
                 .orElse(ResponseEntity.notFound().build());
     }
 }
